ac3acfe92ebbded4912cc49ff4321514fa6272ce
koldointxausti <koldointxaustii@gmail.com>
2019-10-17 10:16:43 +0200

    landing page sin acabar para usar de plantilla

---
+++ public/js/main.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+	$('nav a').click(function(){
+		let href = $(this).attr('href');
+		$('html, body').animate(
+			{scrollTop:($(href).offset().top)},500
+		);
+	});
+
+	// esta variable guarda el estado de las tiendas (si están mostradas sólo 8 o todas)
+	let tiendasMostradas = false;
+	$('#mostrar-mas-tiendas').click(function(){
+		// si están escondidas las muestra y viceversa
+		$('.tienda:nth-child(n+9)').slideToggle(100);
+		// cambia el estado de la variable al contrario al actual
+		tiendasMostradas = !tiendasMostradas;
+		// cambia el texto del botón dependiendo del estado de la variable
+		$(this).text(tiendasMostradas?'ver menos':'ver todo');
+	});
+
+	// animaciones de scroll al clickar iconos de flechas
+	$('#arrow_down').click(function(){
+		$('html, body').animate(
+			{scrollTop:$('#main').offset().top},500
+		);
+	});
+	$('#arrow_up').click(function(){
+		$('html, body').animate(
+			{scrollTop:0},500
+		);
+	});
+
+	let scrollStyling = ()=>{
+		if($(this).scrollTop() > ($('#main').offset().top - 50)){
+			// la flecha para subir aparecerá cuando se llegue al #main
+			$('#arrow_up').fadeIn(500);
+			// la barra de navegación cambia su estilo
+			$('nav ul').css({
+				'backgroundColor':'rgba(255,255,255,.9)',
+				'boxShadow':'1px 1px 20px gray'
+			});
+		}else{
+			// si no la flecha se esconde
+			$('#arrow_up').fadeOut(500);
+			// la barra de navegación recupera su estilo
+			$('nav ul').css({
+				'backgroundColor':'transparent',
+				'boxShadow':'none'
+			});
+		}
+	}
+
+	// ejecutamos al cargar la página por si se está más abajo del #main
+	scrollStyling();
+	// lo ejecutamos también cuando se hace scroll
+	$(window).scroll(function(){
+		scrollStyling();
+	});
+});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,99 +2,125 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <title>Boulevard</title>
+        <link rel="stylesheet" type="text/css" href="css/styles.css">
+        <script src="lib/jquery-3.4.1.min.js"></script>
+        <script src="js/main.js"></script>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+        <!-- IMÁGENES DEL FONDO -->
+        <!--<img src="img/blob1.svg" id="blob1">-->
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+        <svg class="blob" id="blob1" viewBox="0 0 600 600" xmlns="http://www.w3.org/2000/svg">
+          <g transform="translate(300,300)">
+            <path d="M59.2,-72.2C78.4,-40.1,96.7,-20,114,17.3C131.3,54.7,147.7,109.4,128.5,126C109.4,142.7,54.7,121.3,5.7,115.7C-43.4,110,-86.7,120.1,-121.1,103.4C-155.4,86.7,-180.7,43.4,-187.5,-6.8C-194.4,-57,-182.7,-114.1,-148.4,-146.2C-114.1,-178.4,-57,-185.7,-18.5,-167.2C20,-148.7,40.1,-104.4,59.2,-72.2Z" fill="#419a48"/>
+          </g>
+        </svg>
+        <svg class="blob" id="blob2" viewBox="0 0 600 600" xmlns="http://www.w3.org/2000/svg">
+          <g transform="translate(300,300)">
+            <path d="M148.4,-122.4C177.5,-81.9,176,-21,166.9,45.9C157.8,112.7,141.2,185.7,101.3,201.4C61.5,217.1,-1.6,175.7,-32.3,134.9C-62.9,94,-61.2,53.8,-56.2,26.9C-51.1,-0.1,-42.8,-13.8,-32.8,-49.9C-22.9,-86,-11.5,-144.5,24.1,-163.7C59.7,-182.9,119.4,-162.9,148.4,-122.4Z" fill="#419a48"/>
+          </g>
+        </svg>
+        <svg class="blob" id="blob3" viewBox="0 0 600 600" xmlns="http://www.w3.org/2000/svg">
+          <g transform="translate(300,300)">
+            <path d="M150.6,-161.9C186.6,-114.6,201.3,-57.3,207.8,6.5C214.2,70.2,212.5,140.5,176.5,186.5C140.5,232.5,70.2,254.2,1.4,252.8C-67.4,251.4,-134.8,226.8,-173.3,180.8C-211.8,134.8,-221.4,67.4,-222.9,-1.5C-224.5,-70.5,-217.9,-140.9,-179.4,-188.3C-140.9,-235.6,-70.5,-259.8,-6.6,-253.2C57.3,-246.6,114.6,-209.2,150.6,-161.9Z" fill="#7bdcb5"/>
+          </g>
+        </svg>
+        <svg class="blob" id="blob4" viewBox="0 0 600 600" xmlns="http://www.w3.org/2000/svg">
+            <g transform="translate(300, 300)">
+                <path d="M125.4,-163.3C167.7,-141.8,210.8,-112.2,228.3,-71.2C245.7,-30.2,237.4,22.1,210.7,57.3C184,92.5,138.7,110.6,100.5,133.8C62.3,157.1,31.2,185.5,-5.8,193.5C-42.7,201.5,-85.4,188.9,-118.4,164C-151.4,139,-174.6,101.6,-176.1,64.8C-177.5,28,-157.2,-8.2,-147.7,-51.2C-138.1,-94.1,-139.3,-143.7,-116.5,-171.5C-93.7,-199.2,-46.8,-205.1,-2.6,-201.5C41.5,-197.8,83.1,-184.7,125.4,-163.3Z" fill="#419a48"/>
+            </g>
+        </svg>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+        <nav>
+            <ul>
+                <li><a href="#main">Información</a></li>
+                <li><a href="#tiendas">Tiendas</a></li>
+                <li><a href="#contacto">Datos de contacto</a></li>
+            </ul>
+        </nav>
+
+        <header>
+            <img id="logo" src="img/logo.png" alt="logo">
+            <img id="arrow_down" src="img/arrow_down.svg" alt="arrow_down">
+        </header>
+        <img src="img/arrow_up.svg" id="arrow_up"/>
+        <div id="main">
+            <div id="info">
+                <h1>Boulevard</h1>
+                <p>El Boulevard es un maxicentro comercial situado en la ciudad de Vitoria (Álava)</p>
+                <p>Consta de tres plantas, una gasolinera, cinco zonas de aparcamiento propios y grandes instalaciones, expandidas por su amplio dominio. Se encuentra en el barrio de Zaramaga a una media hora de la famosa calle Paz andando y a unos doce minutos en coche. También tiene un hotel llamado igual que él (Hotel Boulevard) de cuatro estrellas que incluye un Restaurante Ñam y un restaurante chino Fulitu.
+                <br>
+                A tan solo dos minutos andando se encuentra la zona industrial de Gamarra.</p>
             </div>
+            <section>
+                <div id="tiendas">
+                    <div class="tienda">
+                        <img src="img/zara.jpg" alt="zara-logo">
+                    </div>
+                    <div class="tienda">
+                        <img src="img/zara.jpg" alt="zara-logo">
+                    </div>
+                    <div class="tienda">
+                        <img src="img/zara.jpg" alt="zara-logo">
+                    </div>
+                    <div class="tienda">
+                        <img src="img/zara.jpg" alt="zara-logo">
+                    </div>
+                    <div class="tienda">
+                        <img src="img/zara.jpg" alt="zara-logo">
+                    </div>
+                    <div class="tienda">
+                        <img src="img/zara.jpg" alt="zara-logo">
+                    </div>
+                    <div class="tienda">
+                        <img src="img/zara.jpg" alt="zara-logo">
+                    </div>
+                    <div class="tienda">
+                        <img src="img/zara.jpg" alt="zara-logo">
+                    </div>
+                    <div class="tienda">
+                        <img src="img/zara.jpg" alt="zara-logo">
+                    </div>
+                    <div class="tienda">
+                        <img src="img/zara.jpg" alt="zara-logo">
+                    </div>
+                    <div class="tienda">
+                        <img src="img/zara.jpg" alt="zara-logo">
+                    </div>
+                    <div class="tienda">
+                        <img src="img/zara.jpg" alt="zara-logo">
+                    </div>
+
+                    <div class="tienda">
+                        <img src="img/zara.jpg" alt="zara-logo">
+                    </div>
+                    <div class="tienda">
+                        <img src="img/zara.jpg" alt="zara-logo">
+                    </div>
+                </div>
+                <button id="mostrar-mas-tiendas">Ver todo</button>
+            </section>
+            <section id="contacto">
+                <div>
+                    <div>
+                        Horario:
+                        <ul>
+                            <li>Lunes   10:00–24:00</li>
+                            <li>Martes  10:00–24:00</li>
+                            <li>Miércoles   10:00–24:00</li>
+                            <li>Jueves  10:00–24:00</li>
+                            <li>Viernes 10:00–1:30</li>
+                            <li>Sábado  10:00–1:30</li>
+                            <li>Domingo 11:30–24:00</li>
+                        </ul>
+                    </div>
+            
+                </div>
+                <iframe id="mapa" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2924.5147058120165!2d-2.6711807709542152!3d42.86197925297592!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd4fc25aa31fd801%3A0x490b3150779ed227!2sCentro%20Comercial%20El%20Boulevard!5e0!3m2!1ses!2ses!4v1571211960538!5m2!1ses!2ses" frameborder="0" allowfullscreen=""></iframe>
+            </section>
+            <footer>David Belinchon, Koldo Intxausti y Jefry Molina &copy 2019</footer>
         </div>
     </body>
 </html>
