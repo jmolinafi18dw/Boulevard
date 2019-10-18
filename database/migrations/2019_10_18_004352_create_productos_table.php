1d8d7cd0e927cee405c7785908b7798096c83eb0
Belins <dbelinchonal18dw@ikzubirimanteo.com>
2019-10-18 04:30:11 +0200

    Subo las migraciones y la configuracion del .env

---
+++ database/migrations/2019_10_18_004352_create_productos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre', 30);
+            $table->text('descripcion', 1000);
+            $table->string('imagen', 100);
+            $table->boolean('stock')->default(TRUE);
+            $table->integer('id_tienda');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
--- database/migrations/2019_10_18_014236_create_tienda_table.php
+++ database/migrations/2019_10_18_014236_create_tienda_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CreateTiendaTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,12 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email', 50)->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('tienda', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre', 30);
+            $table->text('direccion', 1000);
+            $table->string('logo', 100);
+            $table->string('categoria');
         });
     }
 
@@ -27,6 +29,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('tienda');
     }
 }
